21b8c063fc038b08769105ee45a6eebca8bf9d26
Jerry Feng <jerry.feng41@gmail.com>
2016-09-17 13:02:31 -0400

    call html extraction api

--- js/popup.js
+++ js/popup.js
@@ -88,31 +88,21 @@ function getImageUrl(searchTerm, callback, errorCallback) {
   x.send();
 }
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+function makeExtractRequest(url, callback) {
+  var fullUrl = "http://boilerpipe-web.appspot.com/extract?url=" + url + "?extractor=ArticleExtractor&output=text";
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", fullUrl, true);
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4) {
+      callback(xmlHttp.responseText, xmlHttp.status);
+    }
+  }
+  xmlHttp.send();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
+  makeExtractRequest("http://waitbutwhy.com/2016/09/marriage-decision.html", function(response, statusCode) {
+    console.log(statusCode);
+    console.log(response);
   });
 });
