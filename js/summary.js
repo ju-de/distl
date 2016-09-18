56b24d842b1eb792745e55be25ec9e25ebc532d4
Judy Chen <judy.chen@uwaterloo.ca>
2016-09-17 23:35:44 -0400

    u can rate

--- js/summary.js
+++ js/summary.js
@@ -1,7 +1,7 @@
 function getSummary() {
 
 	var oldLength = 200;
-	var summary = "Summary goes here.";
+	var summary = "Lorem ipsum dolor sit amet, ne duo modo convenire facilisis, no eam scaevola principes laboramus. Sea an possit ancillae. Accusam consequat ius te, vidit oratio labitur eos an. Tamquam singulis ei nam, no pro verterem dissentias. Et cum idque labore omnesque, eros nihil eum ex. Lorem ipsum dolor sit amet, ne duo modo convenire facilisis, no eam scaevola principes laboramus. Sea an possit ancillae. Accusam consequat ius te, vidit oratio labitur eos an. Tamquam singulis ei nam, no pro verterem dissentias. Et cum idque labore omnesque, eros nihil eum ex. Lorem ipsum dolor sit amet, ne duo modo convenire facilisis, no eam scaevola principes laboramus. Sea an possit ancillae. Accusam consequat ius te, vidit oratio labitur eos an. Tamquam singulis ei nam, no pro verterem dissentias. Et cum idque labore omnesque, eros nihil eum ex. Lorem ipsum dolor sit amet, ne duo modo convenire facilisis, no eam scaevola principes laboramus. Sea an possit ancillae. Accusam consequat ius te, vidit oratio labitur eos an. Tamquam singulis ei nam, no pro verterem dissentias. Et cum idque labore omnesque, eros nihil eum ex. Lorem ipsum dolor sit amet, ne duo modo convenire facilisis, no eam scaevola principes laboramus. Sea an possit ancillae. Accusam consequat ius te, vidit oratio labitur eos an. Tamquam singulis ei nam, no pro verterem dissentias. Et cum idque labore omnesque, eros nihil eum ex. Lorem ipsum dolor sit amet, ne duo modo convenire facilisis, no eam scaevola principes laboramus. Sea an possit ancillae. Accusam consequat ius te, vidit oratio labitur eos an. Tamquam singulis ei nam, no pro verterem dissentias. Et cum idque labore omnesque, eros nihil eum ex.";
 	var newLength = summary.length;
 
 	// set length stats
@@ -20,9 +20,25 @@ function onCopy() {
 
 }
 
+function thumbsUp() {
+
+	document.getElementById('up').style.color = "#7DB4B5";
+	document.getElementById('down').disabled = true;
+
+}
+
+function thumbsDown() {
+
+	document.getElementById('down').style.color = "#7DB4B5";
+	document.getElementById('up').disabled = true;
+
+}
+
 window.onload = function() {
 
   getSummary();
   document.getElementById("copy").addEventListener("click", onCopy);
+  document.getElementById("up").addEventListener("click", thumbsUp);
+  document.getElementById("down").addEventListener("click", thumbsDown);
 
 }
